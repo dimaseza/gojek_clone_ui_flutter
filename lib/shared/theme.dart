cf96c5ecbfd482622aea5e40dc8378d783170e03
dimaseza <dimasandre18@gmail.com>
2020-11-09 19:55:55 +0700

    first commit adding assets and color

---
+++ lib/shared/theme.dart
@@ -0,0 +1,33 @@
+part of 'shared.dart';
+
+// PRIMARY COLOR
+Color primaryColorGreen50 = Color(0xFF31B057);
+Color primaryColorBlack50 = Color(0xFF25282B);
+
+// SECONDARY COLOR
+Color secondaryColorBlue50 = Color(0xFF388BF2);
+Color secondaryColorRed50 = Color(0xFFE52A34);
+Color secondaryColorYellow50 = Color(0xFFFBAF18);
+Color secondaryColorOrange50 = Color(0xFFFC8338);
+
+// FONT STYLE
+TextStyle blackFontStyle12 = TextStyle(
+  color: primaryColorBlack50,
+  fontSize: 12,
+);
+TextStyle blackFontStyle14 = TextStyle(
+  color: primaryColorBlack50,
+  fontSize: 14,
+);
+TextStyle blackFontStyle16 = TextStyle(
+  color: primaryColorBlack50,
+  fontSize: 16,
+);
+TextStyle blackFontStyle18 = TextStyle(
+  color: primaryColorBlack50,
+  fontSize: 18,
+);
+TextStyle blackFontStyle20 = TextStyle(
+  color: primaryColorBlack50,
+  fontSize: 20,
+);
